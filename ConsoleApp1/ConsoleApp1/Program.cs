914d5ae3c1ea4dfa3688527f62dae1aed0675a9e
igormlynarczuk <igormlynarczuk@pjwstk.edu.pl>
2024-03-18 22:06:10 +0100

    Method average added

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -3,3 +3,11 @@
 Console.WriteLine("MODYFIKACJA 1");
 Console.WriteLine("MODYFIKACJA 2");
 Console.WriteLine("MODYFIKACJA 3");
+
+static double average(int[] tab){
+    double result=0;
+    for(int i=0; i<tab.Length; i++){
+        result += tab[i];
+    }
+    return result/tab.Length
+}
